dd875db3ce76ba2eb150ddfaaa216c8abb0836d8
wegetdata <drdeathishere@hotmail.com>
2016-11-24 15:49:08 -0500

    Added Uplink and pickup with Q key

---
+++ Assets/BlueFiles/Uplink.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uplink : MonoBehaviour {
+	
+	
+	
+	
+	public AudioClip pickup;
+	public AudioClip dropdown;
+	public GameObject blueft;
+	void Update () {
+		
+		if (Input.GetKeyDown (",")) {
+						//drop item code
+						GetComponent<AudioSource>().PlayOneShot (dropdown, 1.0f);
+						transform.GetComponent<Renderer>().enabled = true;
+						transform.parent = null;
+				} 
+	
+		
+	}
+	
+	
+	
+	
+
+	
+	
+	void  OnTriggerEnter(Collider player) {
+		
+		/*if (!hasBox){
+				
+				hasBox = true;
+				
+				box.transform.parent = player.transform;
+				
+			}else{
+				
+				Destroy(box.gameObject);
+				
+			}*/
+		
+	}
+	
+	
+	
+	
+	void OnTriggerStay(Collider other){
+		bool canClone = true;
+		
+		if (Input.GetKeyDown ("z")) {
+			//drop item code
+			GetComponent<AudioSource>().PlayOneShot (dropdown, 1.0f);
+			transform.GetComponent<Renderer>().enabled = true;
+			transform.parent = null;
+		} 
+		
+		
+
+		
+		
+		
+		if(Input.GetKeyDown("q")){
+			
+			if (other.tag == "Player"){ // only be picked by the player!
+				
+				transform.parent = Camera.main.transform; 
+				transform.GetComponent<Renderer>().enabled = false;// pick this object...
+				GetComponent<AudioSource>().PlayOneShot(pickup, 1.0f);
+				
+			}
+			
+			
+		}
+		
+	}
+}	
