c67957cfa69668c8b2c578225772380f89217d19
d4rksh4de <d4rksh4de@gmail.com>
2017-12-10 20:40:52 +0200

    added player name tags over player tanks

---
+++ Assets/PlayerInfoPanelController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Wulfram3 {
+    public class PlayerInfoPanelController : Photon.PunBehaviour {
+        private GameObject target;
+        private Vector3 pos;
+
+        public Text playerNameText;
+
+        // Use this for initialization
+        void Start() {
+            Canvas canvas = FindObjectOfType<Canvas>();
+            transform.SetParent(canvas.transform);
+            string playerName = target.GetComponent<PhotonView>().owner.NickName;
+            playerNameText.text = playerName;
+        }
+
+        // Update is called once per frame
+        void LateUpdate() {
+            if (target != null) {
+                pos = Camera.main.WorldToScreenPoint(target.transform.position);
+                pos.z = 0;
+                RectTransform rectTransform = GetComponent<RectTransform>();
+                pos.y += 50;
+
+                rectTransform.SetPositionAndRotation(pos, rectTransform.rotation);
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+ 
+        }
+
+        public void SetTarget(GameObject target) {
+            this.target = target;
+        }
+
+    }
+}
