7701e0cc739840810da1e0ef27b106e7c102af71
GotchaLogic <34418630+GotchaLogic@users.noreply.github.com>
2017-12-15 00:31:13 -0500

    Added Cargo/Deployable flak turrets
    Added Cargo/Deployable flak turrets

--- Assets/BlueFiles/Uplink.cs
+++ Assets/BlueFiles/Uplink.cs
@@ -1,32 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Uplink : MonoBehaviour
+public class Cargo : MonoBehaviour
 {
 
     public AudioClip pickup;
     public AudioClip dropdown;
     public GameObject blueft;
     public bool isCreated;
-    public Camera camera;
-    public Camera camera2;
 
-    void Start()
-    {
 
-        camera.enabled = true;
-        camera2.enabled = false;
-    }
 
     void Update()
     {
         //This will toggle the enabled state of the two cameras between true and false each time
-        if (Input.GetKeyUp("v"))
-        {
-            camera.enabled = !camera.enabled;
-            camera2.enabled = !camera2.enabled;
-
-        }
     }
     void OnTriggerEnter(Collider player)
     {
@@ -65,19 +52,7 @@ public class Uplink : MonoBehaviour
 
 
         }
-
-        if (Input.GetKeyDown("m"))
-        {
-
-            if (other.tag == "Ship")
-            { // only be picked by the player!
-
-                GUI.Box(new Rect(50, 180, 150, 50), "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@test");
-
-            }
-
-
-        }
+			
 
 
         if (Input.GetKeyDown(","))
@@ -90,18 +65,10 @@ public class Uplink : MonoBehaviour
                     isCreated = true;
 
                 }
-                if (other.tag == "Ship")
-                {
-                    if (gameObject.tag == "bluegt" && !isCreated)
-                    {
-                        PhotonNetwork.Instantiate("bluegt", new Vector3(transform.position.x, transform.position.y + 1.4f, transform.position.z), Quaternion.identity, 0);
-                        isCreated = true;
-                    }
                     GameObject.Destroy(gameObject);
 
                 }
             }
         }
     }
-
-}
+	
