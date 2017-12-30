8e32e6b7eec8dea132612e69a9dc65f87f1c705f
Daniel Matthews <knight129@gmail.com>
2017-12-30 11:35:30 -0500

    Added autosaver to project
    This is allow autosaving of scenes at an interval and on play, ensuring no lose of work.

---
+++ Assets/Plugins/MHLab/AutoSaver/Editor/AutoSaver.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSaver
+{
+    #region Public members
+    // Flag to enable/disable AutoSaver!
+    public static bool IsEnabled = true;
+
+    // Flag to enable/disable debug messages!
+    public static bool IsDebugEnabled = true;
+
+    // Flag to enable/disable assets autosaving!
+    public static bool AutosaveAssets = true;
+
+    // Flag to enable/disable on play saving!
+    public static bool SaveOnPlay = true;
+
+    // The interval in minutes after that AutoSaver will save your scene!
+    public static int AutosaveInterval = 2;
+    #endregion
+
+    #region Private stuff
+    private static DateTime _lastAutosaveTime = DateTime.Now;
+    #endregion
+
+    #region Private methods
+    /// <summary>
+    /// Saves active scene.
+    /// </summary>
+    private static void SaveScene()
+    {
+        Scene activeScene = EditorSceneManager.GetActiveScene();
+        if (activeScene.isDirty)
+        {
+            EditorSceneManager.SaveScene(activeScene);
+            if (AutosaveAssets) AssetDatabase.SaveAssets();
+            if (IsDebugEnabled) Debug.Log("AutoSaver: " + activeScene.name + " saved successfully!");
+        }
+    }
+
+    /// <summary>
+    /// Loads editor preferences.
+    /// </summary>
+    private static void LoadEditorPrefs()
+    {
+        if (EditorPrefs.HasKey("UNITY_AUTOSAVER_prefsExist"))
+        {
+            if (EditorPrefs.HasKey("UNITY_AUTOSAVER_isEnabled"))
+            {
+                AutoSaver.IsEnabled = EditorPrefs.GetBool("UNITY_AUTOSAVER_isEnabled");
+            }
+            if (EditorPrefs.HasKey("UNITY_AUTOSAVER_isDebugEnabled"))
+            {
+                AutoSaver.IsDebugEnabled = EditorPrefs.GetBool("UNITY_AUTOSAVER_isDebugEnabled");
+            }
+            if (EditorPrefs.HasKey("UNITY_AUTOSAVER_autosaveAssets"))
+            {
+                AutoSaver.AutosaveAssets = EditorPrefs.GetBool("UNITY_AUTOSAVER_autosaveAssets");
+            }
+            if (EditorPrefs.HasKey("UNITY_AUTOSAVER_autosaveInterval"))
+            {
+                AutoSaver.AutosaveInterval = EditorPrefs.GetInt("UNITY_AUTOSAVER_autosaveInterval");
+            }
+        }
+    }
+
+    private static void OnUpdate()
+    {
+        if (_lastAutosaveTime.AddMinutes(AutosaveInterval) <= DateTime.Now)
+        {
+            SaveScene();
+            _lastAutosaveTime = DateTime.Now;
+        }
+    }
+
+    private static void OnEnterInPlayMode()
+    {
+        if (SaveOnPlay && !EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
+            SaveScene();
+    }
+    #endregion
+
+    #region Public methods
+    /// <summary>
+    /// Initialize AutoSaver features
+    /// </summary>
+    public static void Initialize()
+    {
+        LoadEditorPrefs();
+        if(IsEnabled)
+            ActivateAutosaver();
+    }
+
+    /// <summary>
+    /// Register AutoSaver's update to Editor's update
+    /// </summary>
+    public static void ActivateAutosaver()
+    {
+        _lastAutosaveTime = DateTime.Now;
+        EditorApplication.update += OnUpdate;
+        EditorApplication.playmodeStateChanged += OnEnterInPlayMode;
+        IsEnabled = true;
+        if (IsDebugEnabled) Debug.Log("AutoSaver: ON");
+    }
+
+    /// <summary>
+    /// Unregister AutoSaver's update to Editor's update
+    /// </summary>
+    public static void DeactivateAutosaver()
+    {
+        EditorApplication.update -= OnUpdate;
+        EditorApplication.playmodeStateChanged -= OnEnterInPlayMode;
+        IsEnabled = false;
+        if (IsDebugEnabled) Debug.Log("AutoSaver: OFF");
+    }
+    #endregion
+
+    #region Constructor
+    static AutoSaver()
+    {
+        Initialize();
+    }
+    #endregion
+}
---
+++ Assets/Plugins/MHLab/AutoSaver/Editor/AutoSaverEditor.cs
@@ -0,0 +1,108 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AutoSaverEditor : EditorWindow
+{
+    [MenuItem("Window/AutoSaver")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(AutoSaverEditor), false, "AutoSaver");
+    }
+
+    void Update()
+    {
+        Repaint();
+    }
+
+    void OnGUI()
+    {
+        #region AutoSaver button
+        if(GUILayout.Button((AutoSaver.IsEnabled) ? "AutoSaver: ON" : "AutoSaver: OFF"))
+        {
+            if (AutoSaver.IsEnabled) AutoSaver.DeactivateAutosaver();
+            else AutoSaver.ActivateAutosaver();
+        }
+        #endregion
+        #region AutoSaver Debug button
+        if (GUILayout.Button((AutoSaver.IsDebugEnabled) ? "AutoSaver Debug: ON" : "AutoSaver Debug: OFF"))
+        {
+            if (AutoSaver.IsDebugEnabled) AutoSaver.IsDebugEnabled = false;
+            else AutoSaver.IsDebugEnabled = true;
+        }
+        #endregion
+
+        #region Assets saver and Save on play buttons
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginVertical();
+        if (GUILayout.Button((AutoSaver.AutosaveAssets) ? "Assets saver: ON" : "Assets saver: OFF"))
+        {
+            if (AutoSaver.AutosaveAssets) AutoSaver.AutosaveAssets = false;
+            else AutoSaver.AutosaveAssets = true;
+        }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical();
+        if (GUILayout.Button((AutoSaver.SaveOnPlay) ? "Save on play: ON" : "Save on play: OFF"))
+        {
+            if (AutoSaver.SaveOnPlay) AutoSaver.SaveOnPlay = false;
+            else AutoSaver.SaveOnPlay = true;
+        }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.EndHorizontal();
+        #endregion
+
+        #region Interval slider
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        AutoSaver.AutosaveInterval = EditorGUILayout.IntSlider("AutoSave Interval:", AutoSaver.AutosaveInterval, 1, 60);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+        #endregion
+
+        #region Save and Delete buttons
+        EditorGUILayout.BeginHorizontal(GUI.skin.box);
+
+        EditorGUILayout.BeginVertical();
+        if (GUILayout.Button("Save prefs"))
+        {
+            SaveEditorPrefs();
+        }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical();
+        if (GUILayout.Button("Delete prefs"))
+        {
+            DeleteEditorPrefs();
+        }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.EndHorizontal();
+        #endregion
+    }
+
+    #region Private methods
+    private void SaveEditorPrefs()
+    {
+        EditorPrefs.SetBool("UNITY_AUTOSAVER_prefsExist", true);
+        EditorPrefs.SetBool("UNITY_AUTOSAVER_isEnabled", AutoSaver.IsEnabled);
+        EditorPrefs.SetBool("UNITY_AUTOSAVER_isDebugEnabled", AutoSaver.IsDebugEnabled);
+        EditorPrefs.SetBool("UNITY_AUTOSAVER_autosaveAssets", AutoSaver.AutosaveAssets);
+        EditorPrefs.SetInt("UNITY_AUTOSAVER_autosaveInterval", AutoSaver.AutosaveInterval);
+
+        Debug.Log("AutoSaver: settings saved!");
+    }
+
+    private void DeleteEditorPrefs()
+    {
+        EditorPrefs.DeleteKey("UNITY_AUTOSAVER_prefsExist");
+        EditorPrefs.DeleteKey("UNITY_AUTOSAVER_isEnabled");
+        EditorPrefs.DeleteKey("UNITY_AUTOSAVER_isDebugEnabled");
+        EditorPrefs.DeleteKey("UNITY_AUTOSAVER_autosaveAssets");
+        EditorPrefs.DeleteKey("UNITY_AUTOSAVER_autosaveInterval");
+
+        Debug.Log("AutoSaver: settings deleted!");
+    }
+    #endregion
+}
