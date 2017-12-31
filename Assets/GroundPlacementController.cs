3e34aecf5af4820782474a23a608c8ff4372862f
GotchaLogic <34418630+GotchaLogic@users.noreply.github.com>
2017-12-31 02:12:10 -0500

    Preview of placement added (press comma)
    Preview of placement added (press comma)

---
+++ Assets/GroundPlacementController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GroundPlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+
+    [SerializeField]
+    private KeyCode newObjectHotkey = KeyCode.Comma;
+
+    private GameObject currentPlaceableObject;
+
+    private float mouseWheelRotation;
+
+    public GameObject placeObject;
+
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if (Input.GetKeyDown(newObjectHotkey))
+        {
+            if (currentPlaceableObject != null)
+            {
+                Destroy(currentPlaceableObject);
+            }
+            else
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+        }
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //RaycastHit hitInfo;
+        //if (Physics.Raycast(ray, out hitInfo))
+        //{
+        currentPlaceableObject.transform.position = placeObject.transform.position;
+         //currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        //}
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        Debug.Log(Input.mouseScrollDelta);
+        mouseWheelRotation += Input.mouseScrollDelta.y;
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+}
+
+
+
+
