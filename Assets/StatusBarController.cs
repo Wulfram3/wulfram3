838435286f272cb7cadec7a3452c1f7ea7e85d41
Daniel Matthews <knight129@gmail.com>
2018-01-03 18:59:53 -0500

    Updated HUD
    Stylized hud some. Moved around health and fuel bars and status text.

---
+++ Assets/StatusBarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBarController : MonoBehaviour {
+
+    public Text statusText1;
+
+    public Text statusText2;
+
+    float pingCount;
+    float frameCount;
+    // Use this for initialization
+    IEnumerator Start () {
+        while (true)
+        {
+            if (Time.timeScale == 1)
+            {
+                yield return new WaitForSeconds(0.1f);
+
+                pingCount = PhotonNetwork.GetPing();
+                frameCount = Mathf.Round((1 / Time.deltaTime));
+                statusText1.text = "Ping:" + pingCount + " ~ " + "FPS:" + frameCount;
+            }
+            else
+            {
+                statusText1.text = "Ping:pause ~ FPS:pause";
+            }
+
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
